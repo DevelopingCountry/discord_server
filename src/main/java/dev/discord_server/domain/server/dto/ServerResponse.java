2bd0cb637d55bc3ce47830e8fe37d31950880521
Lwonbin <lwb9036@gmail.com>
2025-04-08 22:38:47 +0900

    feat: oauth 로직 수정

--- src/main/java/dev/discord_server/domain/server/dto/ServerResponse.java
+++ src/main/java/dev/discord_server/domain/server/dto/ServerResponse.java
@@ -4,6 +4,8 @@ import dev.discord_server.domain.server.entity.Server;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 /**
  * DTO 이름은 domain이름 + request OR response로 정한다.
  * 각자에 필요한 필드들 작성
@@ -12,11 +14,17 @@ import lombok.Getter;
 @Getter
 @Builder
 public class ServerResponse {
+    private Long id;
     private String name;
+    private String image;
+    private LocalDateTime createdAt;
+
 
     public static ServerResponse toResponseDto(Server server) {
         return ServerResponse.builder()
+                .id(server.getId())
                 .name(server.getName())
+                .image(server.getImage())
                 .build();
     }
 }
