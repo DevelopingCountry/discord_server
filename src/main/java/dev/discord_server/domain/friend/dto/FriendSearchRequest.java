18d7d514f89902141f6e7bbf1cd5038d30c5c6ce
sungjoon <dkdldrha@naver.com>
2025-05-13 01:36:20 +0900

    기본생성자 추가 [#43]

--- src/main/java/dev/discord_server/domain/friend/dto/FriendSearchRequest.java
+++ src/main/java/dev/discord_server/domain/friend/dto/FriendSearchRequest.java
@@ -2,9 +2,12 @@ package dev.discord_server.domain.friend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+//이거 트러블슈팅 왜 no 안쓰면 에러듬??
 public class FriendSearchRequest {
     private String nickName;
 }
