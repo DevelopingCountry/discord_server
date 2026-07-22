a355fd8d7341f382fa33c48e7ec2b51e1bb4f146
kimsungjoon <tjdwns6522@uos.ac.kr>
2026-07-22 15:52:58 +0900

    socket 주석

--- src/main/java/dev/discord_server/config/WebSocketEventListener.java
+++ src/main/java/dev/discord_server/config/WebSocketEventListener.java
@@ -23,8 +23,8 @@ public class WebSocketEventListener {
     public void handleConnect(SessionConnectedEvent event) {
         Principal user = event.getUser();
         if (user != null) {
-            redisTemplate.opsForSet().add(ONLINE_KEY, user.getName());
-            friendService.notifyFriendsPresenceChange(Long.parseLong(user.getName()), true);
+            redisTemplate.opsForSet().add(ONLINE_KEY, user.getName()); // 접속 시(로그인) redis에 저장
+            friendService.notifyFriendsPresenceChange(Long.parseLong(user.getName()), true); // 친구들에게 접속했다고 알림
         }
     }
 
@@ -32,8 +32,8 @@ public class WebSocketEventListener {
     public void handleDisconnect(SessionDisconnectEvent event) {
         Principal user = event.getUser();
         if (user != null) {
-            redisTemplate.opsForSet().remove(ONLINE_KEY, user.getName());
-            friendService.notifyFriendsPresenceChange(Long.parseLong(user.getName()), false);
+            redisTemplate.opsForSet().remove(ONLINE_KEY, user.getName()); // !접속 시(로그아웃) redis에서 삭제
+            friendService.notifyFriendsPresenceChange(Long.parseLong(user.getName()), false); // 친구들에게 로그아웃 했다고 알림
         }
     }
 }
